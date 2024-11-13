2d3625d77a9800b1b7327baf3b9291cd3410d16e
Marko Nemec <mnemec31@gmail.com>
2024-11-13 16:44:06 +0100

    Add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import create_db_and_tables
 from app.routes.main import api_router
@@ -6,4 +7,20 @@ from app.routes.main import api_router
 create_db_and_tables()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
