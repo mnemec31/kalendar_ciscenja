62923a0a452ad41f54a8cb39c9217d91a2f8a30a
Marko Nemec <mnemec31@gmail.com>
2024-11-16 11:35:09 +0100

    Don't allow username or password shorter than 3 chars

--- backend/app/routes/login.py
+++ backend/app/routes/login.py
@@ -34,6 +34,12 @@ async def login_for_access_token(
 
 @router.post("/register", response_model=UserPublic)
 async def register(session: SessionDep, user_create: UserCreate):
+    if len(user_create.username) < 3 or len(user_create.password) < 3:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username and password must be at least 3 characters long",
+        )
+
     user_in_db = crud.get_user_by_username(session, user_create.username)
     if user_in_db is not None:
         raise HTTPException(
